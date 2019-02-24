d0f45a965e00eea6e7cf6bca80c5b00dda202538
Vivek Singh <viveksingh.lp@gmail.com>
2019-02-24 22:43:51 +0530

    Add Google Authentication

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
+
+// Load Routes
+const auth = require('./routes/auth');
+
+// Passport Config
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => {
     res.send('It works!');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://Vivek:vivek123@ds349455.mlab.com:49455/storybooks-dev',
+    googleClientID: '732700565627-qiq3rf0a2ub734a0nsmggk8thgt2779n.apps.googleusercontent.com',
+    googleClientSecret: 'ZIHSoIVVs7LTaIjVX1wIlvzo'
+}
